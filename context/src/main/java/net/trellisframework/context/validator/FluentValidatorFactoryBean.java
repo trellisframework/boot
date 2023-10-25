6b42ed73dbe96e7d2c39792b1400a3f47967a1ca
payam-aibees <payam@ai-bees.io>
2023-10-25 16:46:03 +0330

    Task -- Description  - bug fixed FluentValidatorFactoryBean

--- context/src/main/java/net/trellisframework/context/validator/FluentValidatorFactoryBean.java
+++ context/src/main/java/net/trellisframework/context/validator/FluentValidatorFactoryBean.java
@@ -5,12 +5,8 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 public class FluentValidatorFactoryBean extends LocalValidatorFactoryBean {
-    @Override
-    public void validate(Object target, Errors errors) {
-        super.validate(target, errors);
-    }
 
-    /*@Override
+    @Override
     public void validate(Object target, Errors errors) {
         super.validate(target, errors);
         if (!errors.hasErrors() && target instanceof FluentValidator<?>) {
@@ -20,5 +16,6 @@ public class FluentValidatorFactoryBean extends LocalValidatorFactoryBean {
                 errors.reject(String.valueOf(e.getHttpStatus().value()), e.getMessage());
             }
         }
-    }*/
+    }
+
 }
