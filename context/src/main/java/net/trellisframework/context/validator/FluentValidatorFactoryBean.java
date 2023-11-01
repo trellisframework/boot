2d4ae5ca4f88bdd8576e788e1a3c0b85bf31c263
Ahmad-Aibees <ahmad@ai-bees.io>
2023-11-01 16:35:07 +0330

    added: - enabled validator on list api input

--- context/src/main/java/net/trellisframework/context/validator/FluentValidatorFactoryBean.java
+++ context/src/main/java/net/trellisframework/context/validator/FluentValidatorFactoryBean.java
@@ -4,11 +4,19 @@ import net.trellisframework.http.exception.HttpException;
 import org.springframework.validation.Errors;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+import java.util.ArrayList;
+
 public class FluentValidatorFactoryBean extends LocalValidatorFactoryBean {
 
     @Override
     public void validate(Object target, Errors errors) {
-        super.validate(target, errors);
+        if (target instanceof ArrayList objects) {
+            for (Object o : objects) {
+                super.validate(o, errors);
+            }
+        } else {
+            super.validate(target, errors);
+        }
         if (!errors.hasErrors() && target instanceof FluentValidator<?>) {
             try {
                 ((FluentValidator<?>) target).execute();
