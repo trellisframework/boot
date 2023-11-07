ad8a459416ad9c5934874627d0cfb86d8610ffc5
Ahmad-Aibees <ahmad@ai-bees.io>
2023-11-07 17:13:08 +0330

    added: - validate Iterable, Map, and Array - validate nested Iterable, Map, and Array (Ex. List<Map<?, ?>>)

--- context/src/main/java/net/trellisframework/context/validator/FluentValidatorFactoryBean.java
+++ context/src/main/java/net/trellisframework/context/validator/FluentValidatorFactoryBean.java
@@ -3,22 +3,39 @@ package net.trellisframework.context.validator;
 import org.springframework.validation.Errors;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+import java.util.stream.StreamSupport;
 
 public class FluentValidatorFactoryBean extends LocalValidatorFactoryBean {
 
     @Override
     public void validate(Object target, Errors errors) {
-        if (target instanceof ArrayList objects) {
-            for (Object o : objects) {
-                super.validate(o, errors);
+        Deque<Object> stack = new ArrayDeque<>();
+        stack.push(target);
+        while (!stack.isEmpty()) {
+            Object current = stack.pop();
+            if (current instanceof Iterable || current instanceof Map<?, ?> || current instanceof Object[]) {
+                Object[] objects;
+                if (current instanceof Iterable<?> iterable) {
+                    objects = StreamSupport.stream(iterable.spliterator(), false).toArray(Object[]::new);
+                } else if (current instanceof Map<?, ?> map) {
+                    objects = map.values().toArray();
+                } else {
+                    objects = (Object[]) current;
+                }
+                for (Object o : objects) {
+                    stack.push(o);
+                }
+            } else {
+                super.validate(current, errors);
             }
-        } else {
-            super.validate(target, errors);
         }
         if (!errors.hasErrors() && target instanceof FluentValidator<?>) {
             ((FluentValidator<?>) target).execute();
         }
     }
+
 
 }
