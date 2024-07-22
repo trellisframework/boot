5cfff9c004fa38f3047f29807a103dc97e9bef68
payam-aibees <payam@ai-bees.io>
2024-07-22 05:17:37 +0000

    Task -- Description  - add ordinal to NumberUtil

--- util/src/main/java/net/trellisframework/util/number/NumberUtil.java
+++ util/src/main/java/net/trellisframework/util/number/NumberUtil.java
@@ -48,4 +48,12 @@ public class NumberUtil {
         return ret;
     }
 
+    public static String ordinal(int i) {
+        String[] suffixes = new String[] { "th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th" };
+        return switch (i % 100) {
+            case 11, 12, 13 -> i + "th";
+            default -> i + suffixes[i % 10];
+        };
+    }
+
 }
