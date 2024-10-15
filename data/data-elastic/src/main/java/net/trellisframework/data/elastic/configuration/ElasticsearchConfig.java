4c8ff007bcc31c275ee804bb077c66015a2dd394
payam <payam@ai-bees.io>
2024-10-15 09:46:57 +0330

    Task -- Description  - skip ssl certificate on elastic

--- data/data-elastic/src/main/java/net/trellisframework/data/elastic/configuration/ElasticsearchConfig.java
+++ data/data-elastic/src/main/java/net/trellisframework/data/elastic/configuration/ElasticsearchConfig.java
@@ -4,6 +4,7 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import lombok.SneakyThrows;
 import net.trellisframework.core.application.ApplicationContextProvider;
 import net.trellisframework.data.elastic.constant.Messages;
 import net.trellisframework.http.exception.NotFoundException;
@@ -11,14 +12,16 @@ import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.config.RequestConfig;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.TrustAllStrategy;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import javax.net.ssl.SSLContext;
 import java.time.Duration;
 import java.util.Optional;
 
@@ -36,6 +39,7 @@ public class ElasticsearchConfig {
         return properties;
     }
 
+    @SneakyThrows
     public static ElasticsearchClient getInstance() {
         if (client == null) {
             ElasticsearchProperties property = getProperties();
@@ -44,10 +48,14 @@ public class ElasticsearchConfig {
             final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
             credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(property.getUsername(), property.getPassword()));
             String uri = property.getUris().stream().findFirst().orElseThrow(() -> new NotFoundException(Messages.ELASTIC_CONFIG_NOT_FOUND));
+            SSLContext ssl = SSLContextBuilder.create().loadTrustMaterial(null, new TrustAllStrategy()).build();
             RestClient restClient = RestClient.builder(HttpHost.create(uri))
                     .setRequestConfigCallback(x -> x.setConnectTimeout(ConnectionTimeout).setSocketTimeout(socketTimeout))
-                    .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider))
-
+                    .setHttpClientConfigCallback(httpClientBuilder ->
+                            httpClientBuilder
+                                    .setDefaultCredentialsProvider(credentialsProvider)
+                                    .setSSLContext(ssl)
+                                    .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE))
                     .build();
 
             ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
