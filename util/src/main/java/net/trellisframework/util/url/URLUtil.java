c341e324f6f28043ae6f87037d3e762c88d247f7
payam-aibees <payam@ai-bees.io>
2025-02-07 08:35:40 +0000

    Task -- Description  - add multiple method to URLUtil

--- util/src/main/java/net/trellisframework/util/url/URLUtil.java
+++ util/src/main/java/net/trellisframework/util/url/URLUtil.java
@@ -8,35 +8,49 @@ public class URLUtil {
 
     private final static String pattern = "^(https?:\\/\\/)?([\\w.-]+\\.[a-z]{2,6})(:[0-9]{1,5})?(\\/[^\\s]*)?$";
 
+    public static String getBaseUrl(String url) {
+        return url(url).map(x -> {
+            StringBuilder builder = new StringBuilder(x.getProtocol()).append("://").append(x.getHost());
+            if (x.getPort() != -1 && x.getPort() != 80 && x.getPort() != 443) {
+                builder.append(":").append(x.getPort());
+            }
+            return builder.toString();
+        }).orElse(null);
+    }
+
+    public static String getProtocol(String url) {
+        return url(url).map(URL::getProtocol).orElse(null);
+    }
+
     public static String getHost(String url) {
-        return getUrl(url).map(URL::getHost).orElse(null);
+        return url(url).map(URL::getHost).orElse(null);
     }
 
     public static Integer getPort(String url) {
-        return getUrl(url).map(URL::getPort).orElse(null);
+        return url(url).map(URL::getPort).orElse(null);
     }
 
     public static String getPath(String url) {
-        return getUrl(url).map(URL::getPath).orElse(null);
+        return url(url).map(URL::getPath).orElse(null);
     }
 
     public static String getAuthority(String url) {
-        return getUrl(url).map(URL::getAuthority).orElse(null);
+        return url(url).map(URL::getAuthority).orElse(null);
     }
 
     public static String getFile(String url) {
-        return getUrl(url).map(URL::getFile).orElse(null);
+        return url(url).map(URL::getFile).orElse(null);
     }
 
     public static String getQuery(String url) {
-        return getUrl(url).map(URL::getQuery).orElse(null);
+        return url(url).map(URL::getQuery).orElse(null);
     }
 
     public static boolean isValidURL(String url) {
         return Pattern.compile(pattern).matcher(url).matches();
     }
 
-    private static Optional<URL> getUrl(String url) {
+    public static Optional<URL> url(String url) {
         try {
             if (isValidURL(url)) {
                 url = !url.startsWith("http://") && !url.startsWith("https://") ? "https://" + url : url;
