7bafdfae9ccfcda3303fe528c3102754a940331b
payam-aibees <payam@ai-bees.io>
2026-01-05 12:07:40 +0000

    Task -- Description  - upgrade version

--- workflow/workflow-temporal/src/main/java/net/trellisframework/workflow/temporal/util/TypeResolver.java
+++ workflow/workflow-temporal/src/main/java/net/trellisframework/workflow/temporal/util/TypeResolver.java
@@ -1,7 +1,7 @@
 package net.trellisframework.workflow.temporal.util;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import net.trellisframework.core.application.ApplicationContextProvider;
+import net.trellisframework.util.json.JsonUtil;
+import tools.jackson.databind.ObjectMapper;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
@@ -25,24 +25,11 @@ public final class TypeResolver {
         return new Class<?>[0];
     }
 
-    public static Class<?> getReturnType(Class<?> clazz, Class<?> baseInterface) {
-        for (Type type : clazz.getGenericInterfaces()) {
-            if (type instanceof ParameterizedType pt && pt.getRawType() instanceof Class<?> rawClass && baseInterface.isAssignableFrom(rawClass)) {
-                Type[] typeArgs = pt.getActualTypeArguments();
-                if (typeArgs.length > 0 && typeArgs[0] instanceof Class<?> c) {
-                    return c;
-                }
-            }
-        }
-        return Object.class;
-    }
-
     @SuppressWarnings("unchecked")
-    public static <T> T convert(Object value, Class<?> targetType) {
+    public static <T> T convert(Object value, Class<T> targetType) {
         if (value == null || targetType.isInstance(value)) {
             return (T) value;
         }
-        ObjectMapper mapper = ApplicationContextProvider.context.getBean(ObjectMapper.class);
-        return (T) mapper.convertValue(value, targetType);
+        return JsonUtil.toObject(value, targetType);
     }
 }
